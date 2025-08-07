14f4b8559e3a698f5e6e455275c055e356374b57
RUB3N02 <rubyerizo@gmail.com>
2025-08-07 10:39:11 +0000

    por fin

--- src/app.py
+++ src/app.py
@@ -24,9 +24,9 @@ def handle_invalid_usage(error):
 
 
 # Generate sitemap with all your endpoints// Genera un sitemap con todos tus endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+@app.route('/members', methods=['GET'])
+def get_members():
+    return jsonify(jackson_family.get_all_members()), 200
 
 
 @app.route('/members', methods=['GET'])
@@ -36,6 +36,34 @@ def handle_hello():
     response_body = {"hello": "world",
                      "family": members}
     return jsonify(response_body), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+  
+        member = jackson_family.get_member(member_id)
+        if not member:
+            return jsonify({"message": "Member not found"}), 404
+        return jsonify(member), 200
+ 
+
+@app.route('/members', methods=['POST'])
+def add_member():
+  
+        data = request.get_json()
+        required = {"first_name", "age", "lucky_numbers"}
+        if not data or not required.issubset(data.keys()):
+            return jsonify({"message": "Missing or invalid fields"}), 400
+        new_member = jackson_family.add_member(data)
+        return jsonify(new_member), 200
+   
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+  
+    success = jackson_family.delete_member(member_id)
+    if not success:
+        return jsonify({"message": "Member not found"}), 404
+    return jsonify({"done": True}), 200
 
 
 
--- src/datastructures.py
+++ src/datastructures.py
@@ -16,31 +16,59 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
     # This method generates a unique incremental ID //Este método genera un ID único e incremental.
     def _generate_id(self):
-        generated_id = self._next_id
+        id_ = self._next_id
         self._next_id += 1
-        return generated_id
+        return id_
 
     def add_member(self, member):
         ## You have to implement this method// Tienes que implementar este método.
         ## Append the member to the list of _members // Añadir el miembro a la lista _members.
-        pass
-     
+        new_id = self._generate_id()
+        member_to_add = {
+            "id": new_id,
+            "first_name": member["first_name"],
+            "last_name": self.last_name,
+            "age": member["age"],
+            "lucky_numbers": member["lucky_numbers"],
+        }
+        self._members.append(member_to_add)
+        return member_to_add
 
     def delete_member(self, id):
         ## You have to implement this method// Tienes que implementar este método.
         ## Loop the list and delete the member with the given id //Recorre la lista y elimina el miembro con el id dado.
-        pass
+        for i, m in enumerate(self._members):
+            if m["id"] == id:
+                del self._members[i]
+                return True
+        return False
 
     def get_member(self, id):
         ## You have to implement this method// Tienes que implementar este método.
         ## Loop all the members and return the one with the given id //Recorre todos los miembros y devuelve aquel cuyo id coincida con el proporcionado.
-        pass
-
+        for m in self._members:
+            if m["id"] == id:
+                return m
+        return None
     # This method is done, it returns a list with all the family members// Este método está listo; devuelve una lista con todos los miembros de la familia. 
     def get_all_members(self):
         return self._members
